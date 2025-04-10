1ba9674397f5365c14498f4623e3c66b0bde7e53
joo <jooooyeonkim@gmail.com>
2025-04-10 13:48:30 +0900

    환경설정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { FaMapMarked } from "react-icons/fa";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='bg-slate-500'>
+          Edit <code>src/App.js</code> and save to reload.<FaMapMarked />
         </p>
         <a
           className="App-link"
